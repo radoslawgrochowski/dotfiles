fc92a6cc0f63f932c65d09a0524e54459afa8fc3
Radosław Grochowski <rg@fard.pl>
2022-05-09 22:52:20 +0200

    fix(polybar): styling changes

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -2,10 +2,13 @@
 
 # Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use 
-# polybar-msg cmd quit
 
-# Launch Polybar, using default config location ~/.config/polybar/config.ini
-polybar example 2>&1 | tee -a /tmp/polybar.log & disown
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example 2>&1 | tee -a /tmp/polybar.log & disown
+  done
+else
+  polybar --reload example &
+fi
 
 echo "Polybar launched..."
