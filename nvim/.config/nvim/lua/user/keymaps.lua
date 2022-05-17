d405ab2dfb30d4ef110be237ed01e1c612e9c383
Radosław Grochowski <rg@fard.pl>
2022-05-17 12:50:53 +0200

    feat: yank/paste system clipboard keymaps

--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -5,3 +5,12 @@ local keymap = require 'lib.utils'.keymap
 keymap('n', '<leader>', '<NOP>')
 keymap('n', '<leader><F5>', '<cmd>so $MYVIMRC<cr>')
 
+-- clipboard
+keymap('v', '<leader>y', '"+y')
+keymap('n', '<leader>Y', '"+yg_')
+keymap('n', '<leader>y', '"+y')
+
+keymap('v', '<leader>p', '"+p')
+keymap('v', '<leader>P', '"+P')
+keymap('n', '<leader>p', '"+p')
+keymap('n', '<leader>P', '"+P')
