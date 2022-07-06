07c5fb6e008e180f3382e0f40790b1accc0cd0fc
Radosław Grochowski <rg@fard.pl>
2022-07-06 15:05:44 +0200

    feat: move autostart to /scripts

---
+++ scripts/scripts/autostart.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "autostart.sh"
+
+nohup spotify &
+nohup shutter --min_at_startup &
+nohup picom -b &
+
+echo "looking for additional autostart files:"
+for f in ~/scripts/*.autostart.sh; do
+  [ -e "$f" ] || continue
+  echo "found $f"
+  sh $f
+done
