39b66c09a207e7a07d404e56ae13d2e7efe77829
Radosław Grochowski <rg@fard.pl>
2022-10-28 16:08:07 +0200

    fix(nvim): swap deprecated capabilities function

--- nvim/.config/nvim/lua/user/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/user/plugins/lspconfig.lua
@@ -51,7 +51,7 @@ local handlers = {
 
 
 -- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
