233ddbf13887b01e096544198f89dd9cf29d8d09
Radosław Grochowski <rg@fard.pl>
2023-01-24 19:40:18 +0100

    fix(nixos): disable autostart

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -2,8 +2,8 @@
 echo "autostart.sh"
 
 # Fixme: this needs to check if programs aren't already open
-nohup spotify &
-nohup shutter --min_at_startup &
+#nohup spotify &
+#nohup shutter --min_at_startup &
 
 echo "looking for additional autostart files:"
 for f in ~/scripts/*.autostart.sh; do
