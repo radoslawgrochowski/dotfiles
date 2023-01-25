bea6ff46ecef59c87e6a606696851b17227e998c
Radosław Grochowski <rg@fard.pl>
2023-01-25 18:53:08 +0100

    feat(nvim): remove nvim-tree defaults

--- programs/nvim/nvim-tree.lua
+++ programs/nvim/nvim-tree.lua
@@ -5,11 +5,6 @@ vim.g.loaded_netrwPlugin = 1
 require'nvim-tree'.setup {
   disable_netrw       = true,
   hijack_netrw        = true,
-  open_on_setup       = false,
-  ignore_ft_on_setup  = {},
-  open_on_tab         = false,
-  hijack_cursor       = false,
-  update_cwd          = false,
   hijack_directories  = {
     enable = true,
     auto_open = true,
@@ -19,23 +14,9 @@ require'nvim-tree'.setup {
     show_on_dirs = true,
   },
   actions = {
-    use_system_clipboard = true,
-    change_dir = {
-      enable = true,
-      global = false,
-      restrict_above_cwd = false,
-    },
     open_file = {
       quit_on_open = true,
       resize_window = true,
-      window_picker = {
-        enable = true,
-        chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
-        exclude = {
-          filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
-          buftype = { "nofile", "terminal", "help" },
-        },
-      },
     },
   },
 }
