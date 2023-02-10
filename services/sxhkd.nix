39ff39f0342a7023e704a1fe5d4992e7d4553489
Radosław Grochowski <rg@fard.pl>
2023-02-10 16:11:23 +0100

    fix(nixos): rename sxkhd file

--- services/sxhkd.nix
+++ services/sxhkd.nix
@@ -85,10 +85,6 @@
       # contract a window by moving one of its side inward
       "super + alt + shift + {h,j,k,l}" = "bspc node -z {right -20 0,top 0 20,bottom 0 -20,left 20 0}";
       "super + {Left,Down,Up,Right}" = "bspc node -v {-20 0,0 20,0 -20,20 0}";
-
-      ## Desktop management
-      "super + equal" = "bspc monitor -a \"$(rofi -dmenu -p Workspace -i -no-fixed-num-lines)\"";
-      "super + minus" = "bspc desktop -r";
     };
   };
 }
