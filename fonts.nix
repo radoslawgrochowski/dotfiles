bc94692b8064744c92d0cd3debc7aa0361ffeab8
Radosław Grochowski <rg@fard.pl>
2023-02-13 21:01:50 +0100

    fix(nixos): reduce font list

--- fonts.nix
+++ fonts.nix
@@ -1,6 +1,17 @@
 { config, pkgs, inputs, ... }:
 
 {
-  home.packages = [ pkgs.nerdfonts ];
+  home.packages = [
+    (pkgs.nerdfonts.override {
+      fonts = [
+        "FiraCode"
+        "FiraMono"
+        "NerdFontsSymbolsOnly"
+        "IBMPlexMono"
+        "JetBrainsMono"
+      ];
+    })
+  ];
   fonts.fontconfig.enable = true;
 }
+
