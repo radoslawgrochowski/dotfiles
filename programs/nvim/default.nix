992f05c829375c1c91ee4112fd063d2cddeea590
Radosław Grochowski <rg@fard.pl>
2023-02-19 14:00:22 +0100

    fix(nvim): replace vim-projectionist with project.nvim

--- programs/nvim/default.nix
+++ programs/nvim/default.nix
@@ -80,8 +80,15 @@
       }
 
       # Movement
-      vim-rooter
-      vim-lastplace
+
+      ## need to test this more, it seems broken a bit
+      {
+        plugin = project-nvim;
+        type = "lua";
+        config = ''
+          require("project_nvim").setup{}
+        '';
+      }
       vim-projectionist
       {
         plugin = vim-test;
--- programs/nvim/nvim-tree.lua
+++ programs/nvim/nvim-tree.lua
@@ -2,17 +2,23 @@ vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
 
 require 'nvim-tree'.setup {
-    disable_netrw      = true,
-    hijack_netrw       = true,
-    hijack_directories = {
+    sync_root_with_cwd  = true,
+    respect_buf_cwd     = true,
+    update_focused_file = {
+        enable = true,
+        update_root = true,
+    },
+    disable_netrw       = true,
+    hijack_netrw        = true,
+    hijack_directories  = {
         enable = true,
         auto_open = true,
     },
-    diagnostics        = {
+    diagnostics         = {
         enable = true,
         show_on_dirs = true,
     },
-    actions            = {
+    actions             = {
         open_file = {
             quit_on_open = true,
             resize_window = true,
