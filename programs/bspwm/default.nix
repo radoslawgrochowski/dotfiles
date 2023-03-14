6bcdf3c09f631d5bd6fc1f18e899f3ce2b517365
Radosław Grochowski <rg@fard.pl>
2023-03-14 17:38:02 +0100

    fix(bspwm): move windows on reload

--- programs/bspwm/default.nix
+++ programs/bspwm/default.nix
@@ -4,7 +4,7 @@
   home.packages = with pkgs; [
     bc
     bsp-layout
-    xcwd
+    xdo
   ];
 
   xsession.windowManager.bspwm = {
@@ -18,29 +18,29 @@
       "pgrep btop || kitty --title btop --class btop btop"
     ];
     extraConfig = ''
-      ${./split.sh}
       bspc config remove_disabled_monitors true
       bspc config remove_unplugged_monitors true
       bspc config merge_overlapping_monitors true 
       bspc config focused_border_color '#9ece6a'
       sh ${./monitor-order.sh}
       sh ${./split.sh}
+
+      xdo id -N Spotify | xargs -I{} -n1 bspc node {} -d 'Music'
+      xdo id -N 'Microsoft Teams - Preview' | xargs -I{} -n1 bspc node {} -d 'Comms'
+      xdo id -N 'btop' | xargs -I{} -n1 bspc node {} -d '^6'
     '';
     rules = {
       "Spotify" = {
-        follow = true;
         rectangle = "1160x760+0+0";
         state = "pseudo_tiled";
         desktop = "Music";
       };
       "Microsoft Teams - Preview" = {
-        follow = true;
         rectangle = "1660x960+0+0";
         state = "pseudo_tiled";
         desktop = "Comms";
       };
       "btop" = {
-        follow = true;
         desktop = "^6";
       };
     };
