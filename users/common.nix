33861d0525beb428d4d3dc4e36e73d675dffffd8
Radosław Grochowski <rg@fard.pl>
2023-03-17 14:39:33 +0100

    feat(nix): open markdown with chrome

--- users/common.nix
+++ users/common.nix
@@ -36,6 +36,18 @@
 
   systemd.user.startServices = true;
 
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/markdown" = "google-chrome.desktop";
+      "text/html" = "google-chrome.desktop";
+      "x-scheme-handler/http" = "google-chrome.desktop";
+      "x-scheme-handler/https" = "google-chrome.desktop";
+      "x-scheme-handler/about" = "google-chrome.desktop";
+      "x-scheme-handler/unknown" = "google-chrome.desktop";
+    };
+  };
+
   programs.nix-index = {
     enable = true;
     enableFishIntegration = true;
