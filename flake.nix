415cc5a794897285507a1a8e3f36d9d2eea4325a
Radosław Grochowski <rg@fard.pl>
2023-03-25 11:54:24 +0100

    feat(nixos): use nvim nightly

--- flake.nix
+++ flake.nix
@@ -10,65 +10,71 @@
     config-wp.url =
       "git+ssh://git@github.com/radoslawgrochowski/nixos-config-wp.git";
     hyprland.url = "github:hyprwm/Hyprland";
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, config-wp, agenix, hyprland, ... }: {
-    nixosConfigurations = {
-      radoslawgrochowski-desktop = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./nix/configuration.nix
-          ./nix/targets/desktop/hardware-configuration.nix
-          ./nix/targets/desktop/target.nix
-          ./home.nix
-          { home-manager.users.radoslawgrochowski = import ./users/home.nix; }
-          ./hyprland.nix
-          ./services/samba.nix
-          ./services/printing.nix
-          ./docker.nix
-        ];
-        specialArgs = { inherit inputs; };
-      };
+  outputs = inputs@{ nixpkgs, home-manager, config-wp, agenix, hyprland, ... }:
+    let
+      overlays = [ inputs.neovim-nightly-overlay.overlay ];
+    in
+    {
+      nixosConfigurations = {
+        radoslawgrochowski-desktop = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            { nixpkgs.overlays = overlays; }
+            ./nix/configuration.nix
+            ./nix/targets/desktop/hardware-configuration.nix
+            ./nix/targets/desktop/target.nix
+            ./home.nix
+            { home-manager.users.radoslawgrochowski = import ./users/home.nix; }
+            ./hyprland.nix
+            ./services/samba.nix
+            ./services/printing.nix
+            ./docker.nix
+          ];
+          specialArgs = { inherit inputs; };
+        };
 
-      radoslawgrochowski-hp = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./nix/configuration.nix
-          ./nix/targets/hp/hardware-configuration.nix
-          ./nix/targets/hp/target.nix
-          ./home.nix
-          { home-manager.users.radoslawgrochowski = import ./users/home.nix; }
-          ./hyprland.nix
-        ];
-        specialArgs = { inherit inputs; };
-      };
+        radoslawgrochowski-hp = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            { nixpkgs.overlays = overlays; }
+            ./nix/configuration.nix
+            ./nix/targets/hp/hardware-configuration.nix
+            ./nix/targets/hp/target.nix
+            ./home.nix
+            { home-manager.users.radoslawgrochowski = import ./users/home.nix; }
+            ./hyprland.nix
+          ];
+          specialArgs = { inherit inputs; };
+        };
 
-      radoslawgrochowski-dell = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./nix/configuration.nix
-          ./nix/targets/dell/hardware-configuration.nix
-          ./nix/targets/dell/target.nix
-          home-manager.nixosModules.home-manager
-          {
-            nixpkgs.overlays = [
-              (final: prev: {
-                nodejs = prev.nodejs-16_x;
-              })
-            ];
-          }
-          ./home.nix
-          ./hyprland.nix
-          {
-            home-manager.users.radoslawgrochowski = import ./users/work.nix;
-          }
-          config-wp.nixosModules.wp
-          agenix.nixosModules.age
-          { age.identityPaths = [ "/home/radoslawgrochowski/.ssh/id_ed25519" ]; }
-          { boot.kernelModules = [ "tun" "ipv6" ]; }
-        ];
-        specialArgs = { inherit inputs; };
+        radoslawgrochowski-dell = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            { nixpkgs.overlays = overlays; }
+            ./nix/configuration.nix
+            ./nix/targets/dell/hardware-configuration.nix
+            ./nix/targets/dell/target.nix
+            home-manager.nixosModules.home-manager
+            {
+              nixpkgs.overlays = [
+                (final: prev: { nodejs = prev.nodejs-16_x; })
+              ];
+            }
+            ./home.nix
+            ./hyprland.nix
+            {
+              home-manager.users.radoslawgrochowski = import ./users/work.nix;
+            }
+            config-wp.nixosModules.wp
+            agenix.nixosModules.age
+            { age.identityPaths = [ "/home/radoslawgrochowski/.ssh/id_ed25519" ]; }
+            { boot.kernelModules = [ "tun" "ipv6" ]; }
+          ];
+          specialArgs = { inherit inputs; };
+        };
       };
     };
-  };
 }
