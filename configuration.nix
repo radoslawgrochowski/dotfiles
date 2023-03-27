b19fd0c3ea6fc1904f491ff084b461943db017ed
Radosław Grochowski <rg@fard.pl>
2023-03-27 21:44:03 +0200

    fix(nixos): enable pipewire

--- configuration.nix
+++ configuration.nix
@@ -56,9 +56,16 @@
   };
 
   # Audio
-  hardware.pulseaudio = {
+  # hardware.pulseaudio = {
+  #   enable = true;
+  #   support32Bit = true;
+  # };
+  security.rtkit.enable = true;
+  services.pipewire = {
     enable = true;
-    support32Bit = true;
+    pulse.enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
   };
   nixpkgs.config.pulseaudio = true;
 
@@ -107,6 +114,7 @@
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
+
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -19,7 +19,6 @@
   home-manager.users.${username} = {
     home.packages = with pkgs; [
       grim
-      pipewire
       slurp
       swaybg
       swaylock
