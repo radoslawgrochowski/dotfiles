95f0a697cad9b3043fa72d8887f5e540f35e17e0
Radosław Grochowski <rg@fard.pl>
2023-05-04 16:34:08 +0200

    fix(dunst): remove notification sound for spotify

--- modules/dunst/default.nix
+++ modules/dunst/default.nix
@@ -6,8 +6,12 @@
     services.dunst = {
       enable = true;
       settings = {
+        global = {
+          enable_posix_regex = true;
+        };
         play_sound = {
-          summary = "*";
+          # http://www.formauri.es/personal/pgimeno/misc/non-match-regex/?word=Spotify
+          appname = "^([^S]|S(S|p(S|o(S|t(S|i(S|fS)))))*([^Sp]|p([^So]|o([^St]|t([^Si]|i([^Sf]|f[^Sy]))))))*(S(S|p(S|o(S|t(S|i(S|fS)))))*(p(o?|ot(i?|if)))?)?$";
           script = toString (
             pkgs.writeShellScript "play_sound" ''
               ${pkgs.pulseaudio}/bin/paplay ${toString (./notification.wav)}
