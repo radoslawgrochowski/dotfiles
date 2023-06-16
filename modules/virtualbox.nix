55237e4cbf175c6a084b85e6d7ef1b1a1a1cf4d4
Radosław Grochowski <rg@fard.pl>
2023-06-16 15:42:45 +0200

    fix: update flake without virtualbox

--- modules/virtualbox.nix
+++ modules/virtualbox.nix
@@ -1,16 +1,16 @@
 { username, pkgs-stable, ... }:
 {
-  virtualisation.virtualbox.host = {
-    enable = true;
-    package = pkgs-stable.virtualbox;
-  };
+  # virtualisation.virtualbox.host = {
+  #   enable = true;
+  #   # package = pkgs-stable.virtualbox;
+  # };
 
   users.extraGroups.vboxusers.members = [ username ];
 
-  home-manager.users.${username} = {
-    home.packages = with pkgs-stable; [
-      vagrant
-      remotebox
-    ];
-  };
+  # home-manager.users.${username} = {
+  #   home.packages = with pkgs-stable; [
+  #     vagrant
+  #     remotebox
+  #   ];
+  # };
 }
