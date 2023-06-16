32cfdc959f22fcba1a30e11571ae824aae8bdbdb
Radosław Grochowski <rg@fard.pl>
2023-06-16 16:00:20 +0200

    fix: add virtualbox again

--- modules/virtualbox.nix
+++ modules/virtualbox.nix
@@ -1,16 +1,7 @@
 { username, pkgs-stable, ... }:
 {
-  # virtualisation.virtualbox.host = {
-  #   enable = true;
-  #   # package = pkgs-stable.virtualbox;
-  # };
-
+  virtualisation.virtualbox.host = {
+    enable = true;
+  };
   users.extraGroups.vboxusers.members = [ username ];
-
-  # home-manager.users.${username} = {
-  #   home.packages = with pkgs-stable; [
-  #     vagrant
-  #     remotebox
-  #   ];
-  # };
 }
