3a70e30a59313677257d5c30cab1256f4478dc20
Radosław Grochowski <rg@fard.pl>
2024-02-13 20:09:19 +0100

    fix(nvim): use homebrew gnu-sed only for nix-darwin

--- modules/nvim/default.nix
+++ modules/nvim/default.nix
@@ -1,31 +1,28 @@
-{ config, lib, username, pkgs, ... }:
+{ config, lib, username, pkgs, options, ... }:
 {
   config = {
-  home-manager.users.${username} = {
-    home.packages = with pkgs;[
-      (unstable.neovim.override {
-        extraPython3Packages = (p: with p;[
-          python-dotenv
-          requests
-          pkgs.unstable.python311Packages.pynvim
-          prompt-toolkit
-        ]);
-      })
-      cargo
-      rustc
-      ripgrep
-    ];
+    home-manager.users.${username} = {
+      home.packages = with pkgs;[
+        (unstable.neovim.override {
+          extraPython3Packages = (p: with p;[
+            python-dotenv
+            requests
+            pkgs.unstable.python311Packages.pynvim
+            prompt-toolkit
+          ]);
+        })
+        cargo
+        rustc
+        ripgrep
+      ];
 
-    home.file."./.config/nvim/" = {
-      source = ./config;
-      recursive = true;
+      home.file."./.config/nvim/" = {
+        source = ./config;
+        recursive = true;
+      };
     };
+  } // lib.optionalAttrs (builtins.hasAttr "homebrew" options) {
+    # needed for spectre plugin on darwin
+    homebrew.brews = [ "gnu-sed" ];
   };
-
-  }# // (lib.mkIf (config.system == "darwin") {
-   #  	# needed for spectre plugin
-   #  	homebrew.brews = [ "gnu-sed" ]; 
-   #     });
-   # } 
-   ;
 }
