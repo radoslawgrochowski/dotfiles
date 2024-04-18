0452e65fa27c4b2a5f77ba5b7ed2392248e98b78
Radosław Grochowski <rg@fard.pl>
2024-04-18 12:15:48 +0200

    fix(nvim): disable copilot extra

--- modules/lazyvim/config/lua/config/lazy.lua
+++ modules/lazyvim/config/lua/config/lazy.lua
@@ -23,7 +23,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.util.project" },
-    { import = "lazyvim.plugins.extras.coding.copilot" },
+    -- { import = "lazyvim.plugins.extras.coding.copilot" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
