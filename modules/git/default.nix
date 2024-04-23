0faaa55fdda3905b9fdb8575389c2956b89a717e
Radosław Grochowski <rg@fard.pl>
2024-04-23 11:56:11 +0200

    feat(git): git-machete

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -2,7 +2,7 @@
 
 {
   home-manager.users.${username} = {
-    home.packages = [ pkgs.git ];
+    home.packages = with pkgs.stable; [ git git-machete ];
     home.file."./.gitconfig".source = ./.gitconfig;
     home.file."./.gitignore".source = ./.gitignore;
   };
