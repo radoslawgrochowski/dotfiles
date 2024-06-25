b8f63b2168411448771820696b06a15a09bb4330
Radosław Grochowski <rg@fard.pl>
2024-06-26 00:22:48 +0200

    chore(nix): update to nixpkgs 24.05

--- flake.nix
+++ flake.nix
@@ -3,9 +3,9 @@
   description = "radoslawgrochowski workstation configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs-master.url = "github:nixos/nixpkgs/master";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nix-darwin = {
       url = "github:LnL7/nix-darwin";
@@ -16,7 +16,7 @@
       inputs.nixpkgs.follows = "nixpkgs-stable";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs-stable";
     };
     flake-utils.url = "github:numtide/flake-utils";
