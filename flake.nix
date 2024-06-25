a8cbaf0af13c1b5da7f178879537f6e597217e64
Radosław Grochowski <rg@fard.pl>
2024-06-25 23:47:56 +0200

    chore: setup cachix

--- flake.nix
+++ flake.nix
@@ -93,4 +93,16 @@
         };
         packages = { nvim-rg = pkgs.nvim-rg; };
       });
+
+  nixConfig = {
+    experimental-features = [ "nix-command" "flakes" ];
+    extra-substituters = [
+      "https://cache.nixos.org"
+      "https://radoslawgrochowski-dotfiles.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "radoslawgrochowski-dotfiles.cachix.org-1:H4HOTwe9bPc+P2z/RVW3E8yBN6MwzRA4Xhv9RDpVu8c="
+    ];
+  };
 }
--- modules/nix.nix
+++ modules/nix.nix
@@ -2,8 +2,16 @@
   nix = {
     registry.nixpkgs.flake = inputs.nixpkgs-stable;
     settings = {
-      experimental-features = "nix-command flakes";
+      experimental-features = [ "nix-command" "flakes" ];
       trusted-users = [ "@admin" "${username}" ];
+      substituters = [
+        "https://cache.nixos.org"
+        "https://radoslawgrochowski-dotfiles.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "radoslawgrochowski-dotfiles.cachix.org-1:H4HOTwe9bPc+P2z/RVW3E8yBN6MwzRA4Xhv9RDpVu8c="
+      ];
     };
   };
 })
