5173e7360ddb8034c8a142203cebd61f8d27679a
Radosław Grochowski <rg@fard.pl>
2024-10-15 18:26:54 +0200

    feat(nvim): supermaven

--- overlays/nixpkgs-local-vim-plugins.nix
+++ overlays/nixpkgs-local-vim-plugins.nix
@@ -27,6 +27,16 @@
               hash = "sha256-++5E0R6zTLIMXMFZbEqwB3DMEwbLifjQFWWfTVWwWCo=";
             };
           };
+        supermaven-nvim = pkgs.vimUtils.buildVimPlugin
+          {
+            name = "supermaven-nvim";
+            src = pkgs.fetchFromGitHub {
+              owner = "supermaven-inc";
+              repo = "supermaven-nvim";
+              rev = "07d20fce48a5629686aefb0a7cd4b25e33947d50";
+              hash = "sha256-1z3WKIiikQqoweReUyK5O8MWSRN5y95qcxM6qzlKMME=";
+            };
+          };
       }
     );
   };
