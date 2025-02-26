2b6e1a5dfef060d41893d8a31da7e71adf6dd23f
Radosław Grochowski <rg@fard.pl>
2025-02-26 18:07:20 +0100

    fix(nvim): disable latex support for render-markdown

--- modules/nvim/nvim/plugin/theme.lua
+++ modules/nvim/nvim/plugin/theme.lua
@@ -3,5 +3,6 @@ local wk = require 'which-key'
 vim.cmd.colorscheme 'tokyonight-night'
 
 require('render-markdown').setup {
+  latex = { enabled = false },
   file_types = { 'markdown', 'markdown.mdx' },
 }
