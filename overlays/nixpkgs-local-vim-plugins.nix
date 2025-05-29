3c08979a328ccb47d51bfcf14901175663b58fe4
Radosław Grochowski <rg@fard.pl>
2025-05-29 11:06:00 +0200

    fix: remove checks for local vim plugins

--- overlays/nixpkgs-local-vim-plugins.nix
+++ overlays/nixpkgs-local-vim-plugins.nix
@@ -16,6 +16,7 @@
             rev = "1e1900b0769324a9675ef85b38f99cca29e203b3";
             sha256 = "1yqg4gnzmlm9h5rcmzv7msjmqna0ffn7gllf5knfkps5ns0ynpyf";
           };
+          doCheck = false;
         };
         nvim-nio = pkgs.vimUtils.buildVimPlugin {
           name = "nvim-nio";
@@ -26,6 +27,7 @@
             rev = "21f5324bfac14e22ba26553caf69ec76ae8a7662";
             sha256 = "1bz5msxwk232zkkhfxcmr7a665la8pgkdx70q99ihl4x04jg6dkq";
           };
+          doCheck = false;
         };
         neotest = pkgs.vimUtils.buildVimPlugin {
           name = "neotest";
@@ -36,6 +38,7 @@
             rev = "dddbe8fe358b05b2b7e54fe4faab50563171a76d";
             sha256 = "0ji65yyfisr1fpfsx8kflb9fl17dmf168wwvqgvp7z24m9v9b3bb";
           };
+          doCheck = false;
         };
         neotest-jest = pkgs.vimUtils.buildVimPlugin {
           name = "neotest-jest";
@@ -46,6 +49,7 @@
             rev = "514fd4eae7da15fd409133086bb8e029b65ac43f";
             sha256 = "1lmz248bzdhggvarikhpr5210mbw9fycks93k719d05sb4l6i2dg";
           };
+          doCheck = false;
         };
         telescope-oil-nvim = pkgs.vimUtils.buildVimPlugin {
           name = "telescope-oil-nvim";
@@ -56,6 +60,7 @@
             rev = "1aaeb1a38a515498f1435d3d308049310b9a5f52";
             sha256 = "0k0a2dsix6sjdv3p10vrxf19klkpgbyvlli16x7fskxnsnaav4mx";
           };
+          doCheck = false;
         };
         parrot-nvim = pkgs.vimUtils.buildVimPlugin {
           name = "parrot-nvim";
@@ -66,6 +71,7 @@
             rev = "3027b481ef58245147408455a13b61608a86885c";
             sha256 = "0ras4vw435pjs5vvy2cfxiga8nlbil88l954ajq85d651wgi9gdj";
           };
+          doCheck = false;
         };
         nvim-vtsls = pkgs.vimUtils.buildVimPlugin {
           name = "nvim-vtsls";
@@ -76,6 +82,7 @@
             rev = "45c6dfea9f83a126e9bfc5dd63430562b3f8af16";
             sha256 = "08ms4yg821dk1qvhs7i3qh51hhn43v4p55yi81bbvk6za7n68bgz";
           };
+          doCheck = false;
         };
         tsc-nvim = pkgs.vimUtils.buildVimPlugin {
           name = "tsc-nvim";
@@ -86,6 +93,7 @@
             rev = "5bd25bb5c399b6dc5c00392ade6ac6198534b53a";
             sha256 = "1s47l4m741d7z9diicqn48b5avk1n3sxx64f8xjr6l0rn9518zsz";
           };
+          doCheck = false;
         };
         dir-telescope-nvim = pkgs.vimUtils.buildVimPlugin {
           name = "dir-telescope-nvim";
@@ -96,6 +104,7 @@
             rev = "805405b9f98dc3470f8676773dc0e6151a9158ed";
             sha256 = "1aqfvlg3p1kln8par7gaqdzra9lsb29c2hcairv2wmn09191kxq7";
           };
+          doCheck = false;
         };
       }
     );
