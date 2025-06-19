084c7d97de128a1567129a49542a09ee7a13c273
Radosław Grochowski <rg@fard.pl>
2025-06-19 14:27:19 +0200

    perf: Increase inotify limits for WSL

--- presets/wsl.nix
+++ presets/wsl.nix
@@ -12,4 +12,10 @@
       xclip
     ];
   };
+
+  boot.kernel.sysctl = {
+    "fs.inotify.max_user_watches" = 1048576;
+    "fs.inotify.max_user_instances" = 1024; 
+    "fs.inotify.max_queued_events" = 32768; 
+  };
 }
