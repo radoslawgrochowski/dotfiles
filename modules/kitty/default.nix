ca97f4e965709c402377ec8b745fca10aa8b935a
Radosław Grochowski <rg@fard.pl>
2025-08-13 22:26:08 +0200

    refactor: remove unused notebook reference

--- modules/kitty/default.nix
+++ modules/kitty/default.nix
@@ -1,22 +1,8 @@
-{ username, pkgs, lib, config, options, ... }:
-let
-  package = pkgs.kitty;
-  kitty = "${package}/bin/kitty";
-  userHome = config.users.users."${username}".home;
-  # TODO: move to notebook module
-  notebookPath = "${userHome}/Projects/notebook";
-in
+{ username, pkgs, ... }:
 {
-  config = {
-    home-manager.users.${username} = {
-      home.packages = [ pkgs.kitty ];
-      home.file."./.config/kitty/kitty.conf".source = ./kitty.conf;
-    };
-  } // lib.optionalAttrs (builtins.hasAttr "skhd" options.services) {
-    services.skhd.skhdConfig = ''
-      hyper - return : ${kitty} --single-instance -d ~
-      hyper - n : ${kitty} --single-instance -T notebook -d ${notebookPath} nvim ${notebookPath}
-    '';
+  home-manager.users.${username} = {
+    home.packages = [ pkgs.kitty ];
+    home.file."./.config/kitty/kitty.conf".source = ./kitty.conf;
   };
 }
 
