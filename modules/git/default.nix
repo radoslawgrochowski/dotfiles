01a0d3289b23f9365e1df610854533ffdea47951
Radosław Grochowski <rg@fard.pl>
2026-01-21 10:42:43 +0100

    feat(git): add worktree utilities

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -1,5 +1,10 @@
 { username, pkgs, ... }:
 
+let
+  clone-for-worktrees = pkgs.writeShellScriptBin "clone-for-worktrees" (
+    builtins.readFile ./clone-for-worktrees.sh
+  );
+in
 {
   home-manager.users.${username} = {
     home.packages = with pkgs; [
@@ -8,6 +13,7 @@
         doCheck = false; # Skip tests - failing in Nix sandbox on macOS
       }))
       git-lfs
+      clone-for-worktrees
     ];
     home.file."./.gitconfig".source = ./.gitconfig;
     home.file."./.gitignore".source = ./.gitignore;
