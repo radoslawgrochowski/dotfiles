f7a39394a444caf21a3e5f557a739e0ad3679224
Radosław Grochowski <rg@fard.pl>
2026-04-14 14:44:25 +0200

    feat(aerospace): migrate from home-manager

--- modules/aerospace/default.nix
+++ modules/aerospace/default.nix
@@ -1,10 +1,17 @@
-{ username, pkgs, ... }:
 {
-  users.users."${username}".packages = [
-    pkgs.unstable.aerospace
-  ];
-
-  home-manager.users.${username} = {
-    home.file."./.aerospace.toml".source = ./aerospace.toml;
+  config,
+  username,
+  pkgs,
+  ...
+}:
+{
+  services.aerospace = {
+    package = pkgs.unstable.aerospace;
+    enable = true;
+    settings = builtins.fromTOML (builtins.readFile ./aerospace.toml);
   };
+
+  users.users."${username}".packages = [
+    config.services.aerospace.package
+  ];
 }
