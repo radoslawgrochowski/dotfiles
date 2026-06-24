598327347e1cfd9904b26ccf46af6052157083f0
Radosław Grochowski <rg@fard.pl>
2026-06-24 19:37:12 +0200

    fix(nix): update flake

--- modules/docker/default.nix
+++ modules/docker/default.nix
@@ -2,4 +2,8 @@
 {
   virtualisation.docker.enable = true;
   users.groups.docker.members = [ username ];
+  # remove after 26.05 update
+  nixpkgs.config.permittedInsecurePackages = [
+    "docker-28.5.2"
+  ];
 }
